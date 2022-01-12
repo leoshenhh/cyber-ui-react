b8396812b8af3276a00d3897eeabf70f9eb3b764
Leo Shen <343206898@qq.com>
2022-01-12 20:42:31 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'production',
     entry: {
@@ -17,5 +18,9 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: "CyberUI",
+        template: 'index.html'
+    })],
 }
