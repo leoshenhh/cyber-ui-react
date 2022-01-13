811fb86ac173d22765d58dde84cae0d2caa408cf
Leo Shen <343206898@qq.com>
2022-01-13 20:49:20 +0800

    Test add button test

---
+++ lib/__test__/button.unit.tsx
@@ -0,0 +1,8 @@
+import renderer from 'react-test-renderer'
+import Button from '../button';
+describe('button',()=>{
+    it('是个div',()=>{
+        const json = renderer.create(<Button/>).toJSON()
+        expect(json).toMatchSnapshot()
+    })
+})
---
+++ lib/__test__/hello.test.tsx
@@ -0,0 +1,3 @@
+test('hello',()=>{
+    expect(1).toEqual(1)
+})
