c03e26020f17c85fed1070bacb16ce8cd2c90ff2
Leo Shen <343206898@qq.com>
2022-01-19 18:08:58 +0800

    Feat add icon onClick

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import './icon.scss'
 const reqSvg = require.context ( '../icons', true, /\.svg$/ )
 reqSvg.keys().map( path => reqSvg ( path ) )
 
 
 interface IconProps {
     name: string;
+    onClick: () => void;
 }
 
 const Icon:React.FunctionComponent<IconProps> = (props)=> {
     return (
-        <span>
-            <svg>
+            <svg className="icon" onClick={props.onClick}>
                 <use xlinkHref={'#' + props.name}/>
             </svg>
-        </span>
     );
 }
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
+const fn = () =>{
+    console.log('111')
+}
 ReactDOM.render(<div>
-    <Icon name="wechat"/>
-    <Icon name="QQ"/>
-    <Icon name="alipay"/>
+    <Icon name="wechat" onClick={fn}/>
 </div>, document.querySelector("#root"));
