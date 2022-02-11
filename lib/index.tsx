70d84916f96172cf6402b61d4642ea4d7b0c3363
Leo Shen <343206898@qq.com>
2022-02-11 19:21:42 +0800

    Build auto npm publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Icon from './icon/icon'
 
-const fn:React.MouseEventHandler = (e) =>{
-    console.log(e)
-    console.log(e.target)
-}
-ReactDOM.render(<div>
-    <Icon className='test1 test2' name="wechat" onClick={fn} onMouseEnter={()=>{console.log('enter')}} onMouseLeave={()=>{console.log('leave')}}/>
-</div>, document.querySelector("#root"));
+export {Icon}
