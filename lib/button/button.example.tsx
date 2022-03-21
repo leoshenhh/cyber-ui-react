6d1f85bc2dd96e34f33c1eb27f6a7d470d25c02e
Leo Shen <343206898@qq.com>
2022-03-21 16:37:45 +0800

    Feat add Button component

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Button from './button'
+import Button from './button';
 
 function ButtonExample() {
-    return (
-        <Button>我是一个button</Button>
-    );
+  return (
+    <>
+      <h1>example1</h1>
+      <Button size='medium'>我是一个button</Button>
+      <h1>example3</h1>
+      <Button size='large'>我是一个button</Button>
+    </>
+
+  );
 }
 
 export default ButtonExample;
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {scopedClassMaker} from '../helper/class-names';
+import './button.scss'
 
-const Button: React.FunctionComponent = (props) => {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'small' | 'medium' | 'large'
+}
+const sc = scopedClassMaker('cyber-button')
+
+const Button: React.FunctionComponent<Props> = (props) => {
+
+  const { className,...rest} = props
   return (
-    <div>
+    <button className={sc(['',props.size])} {...rest}>
       {props.children}
-    </div>
+    </button>
   );
 };
 
