9e69e5cdaded123b1d614a2a50223f12a684b478
Leo Shen <343206898@qq.com>
2022-03-30 21:29:23 +0800

    Feat button compoents props

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Button from './button';
+import PreCode from '../precode/precode';
+import Cyber from '../cyber/cyber';
+
+const code = `
+import React from 'react'
+import { Button } from 'cyber-ui-react'
+
+const demo = () => {
+          <Button size="small">button</Button>
+          <Button size="medium">button</Button>
+          <Button size="large">button</Button>
+}
+
+ReactDOM.render(<Demo />, document.getElementById('root'));
+`;
 
 function ButtonExample() {
   return (
     <>
-      <h1>example1</h1>
-      <Button size='medium'>我是一个button</Button>
-      <h1>example3</h1>
-      <Button size='large'>我是一个button</Button>
+      <PreCode codeHeight="270px" title="button" describe="" code={code}>
+        <div>
+          <Button size="small">button</Button>
+          <Button size="medium">button</Button>
+          <Button size="large">button</Button>
+        </div>
+
+      </PreCode>
     </>
 
   );
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,20 +1,94 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {scopedClassMaker} from '../helper/class-names';
-import './button.scss'
+import './button.scss';
+import {Gradient, handleGradients} from '../cyber/cyber';
+import styled from 'styled-components';
+
+const ButtonAni = styled('span')<{ width: number; height: number; aniName: string }>`
+  @keyframes ${props => props.aniName} {
+    0% {
+      background-position: 0 0;
+    }
+    100% {
+      background-position: ${props => props.width}px ${props => props.height}px;
+    }
+  }
+`;
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: 'small' | 'medium' | 'large'
+  modal?: 'default' | 'primary';
+  size?: 'small' | 'medium' | 'large';
+  angle?: number;
+  gradients?: Array<Gradient>;
+  animate?: boolean;
+  speed?: number;
+  direction?: 'all' | 'row' | 'column'
 }
-const sc = scopedClassMaker('cyber-button')
+
+const sc = scopedClassMaker('cyber-button');
 
 const Button: React.FunctionComponent<Props> = (props) => {
+  const {className, angle, gradients,animate,speed, direction,modal,size,...rest} = props;
+  const [buttonAniID,setButtonAniID] = useState('');
+  const [buttonWidth,setButtonWidth] = useState(0)
+  const [buttonHeight,setButtonHeight] = useState(0)
 
-  const { className,...rest} = props
+  const buttonRef = useRef(null)
+
+  useEffect(()=>{
+    if(direction === 'all'){
+      setButtonHeight(buttonRef.current.scrollHeight)
+      setButtonWidth(buttonRef.current.scrollWidth )
+    }else if(direction === 'row'){
+      setButtonWidth(buttonRef.current.scrollWidth)
+    }else if(direction === 'column'){
+      setButtonHeight(buttonRef.current.scrollHeight)
+    }
+
+    setButtonAniID(`cyberAni${Math.floor(Math.random() * 1000)}`)
+  },[])
+
+
   return (
-    <button className={sc(['',props.size])} {...rest}>
-      {props.children}
+    <button
+      ref={buttonRef}
+      className={sc(['', size])}
+      style={{
+        animation: animate ? `${buttonAniID} ${speed}s infinite linear` : '1s',
+        backgroundImage: `linear-gradient(${angle}deg,${handleGradients(gradients)}`
+      }}
+      {...rest}
+    >
+      <ButtonAni
+        className={sc(['inner',`inner-${size}`])}
+        width={buttonWidth}
+        height={buttonHeight}
+        aniName={buttonAniID}
+        style={{
+          background: modal==='primary'?  'black' :'transparent'
+        }}
+      >
+        {props.children}
+      </ButtonAni>
+
     </button>
   );
+};
+
+Button.defaultProps = {
+  animate: false,
+  size: 'medium',
+  angle: 90,
+  gradients: [
+    {color: '#EA5DAD', percent: 0},
+    {color: '#C2A0FD', percent: 30},
+    {color: '#3BF0E4', percent: 50},
+    {color: '#C2A0FD', percent: 70},
+    {color: '#EA5DAD', percent: 100},
+  ],
+  speed: 1,
+  direction: 'row',
+  modal: 'primary'
 };
 
 export default Button;
